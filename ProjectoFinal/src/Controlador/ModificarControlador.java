4ba2ad67a090304cd5e45fc84bba90cf95114d4c
Ivan Gallardo <ivanmartinezcamacho07@gmail.com>
2025-06-10 19:09:36 +0200

    Enhance ModificarControlador to improve product modification logic and category loading

--- ProjectoFinal/src/Controlador/ModificarControlador.java
+++ ProjectoFinal/src/Controlador/ModificarControlador.java
@@ -1,43 +1,67 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Controlador;
 
 import Model.Categoria;
-
-/**
- *
- * @author ivan-gallardo
- */
-
 import Model.Producte;
 import Vista.ModificarProducte;
+import DAO.CategoriaDAO;
+import DAO.ProducteDAO;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import DAO.CategoriaDAO;
+import java.util.List;
 
 public class ModificarControlador {
-    private DAO.ProducteDAO producteDAO;
-    private Vista.ModificarProducte vista;
+
+    private ProducteDAO producteDAO;
+    private ModificarProducte vista;
+
     public ModificarControlador(ModificarProducte vista) {
         this.vista = vista;
+        this.producteDAO = new ProducteDAO();
     }
-    private ActionListener BotonModificarProducte = new ActionListener() {
+
+    private final ActionListener BotonModificarProducte = new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
-            String nom = vista.getNom();
-            String Cat = vista.getCategoria();
-            int idCategoria = CategoriaDAO.buscarIdNom(Cat);
-            double preu = Double.parseDouble(vista.getPreu());
-            String Tipus = vista.getTipusPreu();
-            int stock = Integer.parseInt(vista.getStock());
-            Producte producteModificat = new Producte(nom, idCategoria, preu, Tipus, stock);
-            producteDAO.modificarProducte(producteModificat);
-    }
-        };
+            try {
+                String nom = vista.getNom();
+                String Cat = vista.getCategoria();
+                int idCategoria = CategoriaDAO.buscarIdNom(Cat);
+
+                Categoria categoria = new Categoria();
+                categoria.setId(idCategoria);
+                categoria.setNom(Cat);
+
+                double preu = Double.parseDouble(vista.getPreu());
+                String Tipus = vista.getTipusPreu();
+                int stock = Integer.parseInt(vista.getStock());
+                boolean oferta = vista.getOferta().equalsIgnoreCase("Oferta");
+
+                Producte producteModificat = new Producte(nom, categoria, preu, Tipus, stock, oferta);
+                producteDAO.modificarProducte(producteModificat);
+
+                javax.swing.JOptionPane.showMessageDialog(vista, "Producte modificat correctament.");
+                vista.dispose();
+
+            } catch (NumberFormatException ex) {
+                javax.swing.JOptionPane.showMessageDialog(vista, "Preu o stock no vàlids.", "Error", javax.swing.JOptionPane.ERROR_MESSAGE);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    };
+
     public void iniciarControlador() {
         vista.getBAcceptar().addActionListener(BotonModificarProducte);
+        cargarCategoriasEnCombo();
         vista.setVisible(true);
+    }
+
+    public void cargarCategoriasEnCombo() {
+        List<Categoria> categorias = CategoriaDAO.llistarCategorias();
+        vista.getComboBoxCategoria().removeAllItems();
+        for (Categoria c : categorias) {
+            vista.getComboBoxCategoria().addItem(c.getNom());
+        }
     }
 }
